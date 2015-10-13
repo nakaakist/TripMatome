d08aa9910f5f796774c3a449687d18b85bdf0cf7
Nariyuki Saito <toshincompos@gmail.com>
2015-10-13 14:16:26 +0900

    changed multiple article fetching method

--- config/article_fetch.rb
+++ config/article_fetch.rb
@@ -9,34 +9,22 @@ require File.expand_path('../boot', __FILE__)
 require File.expand_path('../environment', __FILE__)
 
 def fetch_article(site_name, site_url, xpath, title_url_getter)
-
-  begin
-    prev_fetched_urls = open("article_urls/#{site_name}.json") do |io|
-      JSON.load(io)
-    end
-  rescue => e
-    puts e.message
-    puts e.class
-    prev_fetched_urls = []
+  prev_fetched_urls = []
+  Article.find_by_sql(['select url from articles where website = ?', site_name]).each do |article|
+    prev_fetched_urls.push(article.url)
   end
-
+  p prev_fetched_urls
   client = HTTPClient.new
   html = client.get_content(site_url)
   parsed_html = Nokogiri.parse(html)
-  fetched_urls = []
   parsed_html.xpath(xpath).each do |elem|
     title, url, date = title_url_getter.call(elem)
-    fetched_urls.push(url)
     next if prev_fetched_urls.include?(url)
     unless prev_fetched_urls.empty?
       date = DateTime.now
     end
     article = Article.new({'title'=> title, 'website'=> site_name, 'url'=> url, 'datetime'=> date})
     article.save
-  end
-
-  open("article_urls/#{site_name}.json", "w") do |io|
-    JSON.dump(fetched_urls, io)
   end
 end
 
