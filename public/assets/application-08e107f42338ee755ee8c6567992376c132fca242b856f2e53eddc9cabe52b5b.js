2331d175b3a4d3f51dcf7032e120b949e598b691
xyzsolovers <shun@shun-ichiro-no-MacBook-Air.local>
2015-11-14 07:40:23 +0900

    changed public/assets

--- public/assets/application-08e107f42338ee755ee8c6567992376c132fca242b856f2e53eddc9cabe52b5b.js
+++ public/assets/application-08e107f42338ee755ee8c6567992376c132fca242b856f2e53eddc9cabe52b5b.js
@@ -13607,6 +13607,19 @@ return jQuery;
   });
 
 }).call(this);
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-68849715-1']);
+(function () {
+    var ga = document.createElement('script');
+    ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+$(document).on("pageshow", '[data-role="page"]', function () {
+    var url = location.pathname + location.hash;
+    url ? _gaq.push(['_trackPageview', url]) : _gaq.push(['_trackPageview']);
+});
 $(document).bind("mobileinit", function(){
   $.extend( $.mobile, {
     ajaxEnabled: false
